297207e86c839df30a5e105dd12bfb77c55b98a2
MickaelFP <72392674+MickaelFP@users.noreply.github.com>
2020-11-18 12:59:09 +0100

    Update Puissance4.cpp

--- Puissance4.cpp
+++ Puissance4.cpp
@@ -1,33 +1,68 @@
+
 #include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-int main() {
-    
-    /* int tab [6][7]; */ 
-    cout << "____";
+void show(int a [6][7]) {
     for (int i = 0; i < 6; i++){
-        cout << "____" ;
         
-    }
-        
-    cout << endl; 
-    for (int i = 0; i < 6; i++) {
-      
+        std::cout << "| ";
         for (int j = 0; j < 7; j++){
-            cout << " X |";
+        
+            switch (a[i][j]) {
+            case 0:
+                cout << " ";
+                break;
+            case 1:
+                cout << "X";
+                break;
+            case 2:
+                cout << "O";
+                break;
+            default:
+                break;
+            }
+            cout << " ";
+        }
+        cout << "|" << endl << "|---------------|" << endl;
+    }
+    cout << "  1 2 3 4 5 6 7  " << endl << endl;
+}
+
+int main() {
      
-        }
-        
-        cout << endl;
-        for (int j = 0; j < 7; j++){
-            cout << "____";
-            
-        }
-        
-        cout << endl;
-    }
+    srand(time(NULL));
+    int tab[6][7] = { 0 };
+    show(tab);
     
+    /* test random
+    for (int i = 0; i < 6; i++) {
+        
+        for (int j = 0; j < 7; j++) {
+            tab[i][j] = (rand() % 2) + 1;
+        }
+        
+    } */
+    
+    int colonne;
+    while(1) {
+        
+        cin >> colonne;
+        colonne--;
+        for (int i = 5; i >= 0; i--) {
+            
+            if (tab[i][colonne] == 0) {
+                tab[i][colonne] = 1;
+                break;
+                
+        }
+            
+    }
+    show(tab);
+        
+    }
+
+    return 0;
 }
